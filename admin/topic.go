8f96c7e74d93acd960748fd3f03a3ec7f7639c9a
code-raushan <code-raushan@users.noreply.github.com>
2024-06-17 17:19:00 +0530

    add: topic admin creator

---
+++ admin/topic.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main(){
+	topic := flag.String("topic", "", "Specify the topic name to create")
+	partitions := flag.String("partitions", "1", "Specify the partition count")
+
+	numOfPartitions, err := strconv.Atoi(*partitions)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	flag.Parse()
+
+	if *topic == "" {
+		fmt.Println("Topic name not provided")
+	}else{
+		fmt.Printf("Topic : %s\n", *topic)
+	}
+
+	brokerAddr := "localhost:9092"
+
+	conn, err := kafka.Dial("tcp", brokerAddr)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var controllerConn *kafka.Conn
+
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer controllerConn.Close()
+
+	topicConfigs := []kafka.TopicConfig{
+		{
+			Topic: *topic,
+			NumPartitions: numOfPartitions,
+			ReplicationFactor: 1,
+		},
+	}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Topic %s created", *topic)
+
+}
